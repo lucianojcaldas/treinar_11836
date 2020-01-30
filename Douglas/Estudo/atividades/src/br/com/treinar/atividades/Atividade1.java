0edf4c78ede55324c429afe3ddd1af2954115da0
Treinar <Treinar@MICRO>
2020-01-29 22:45:54 -0200

    Novo

---
+++ Douglas/Estudo/atividades/src/br/com/treinar/atividades/Atividade1.java
@@ -0,0 +1,12 @@
+package br.com.treinar.atividades;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+		int tempo = 135; // minutos
+		int horas = tempo / 60;
+		int minutos = tempo % 60;
+
+		System.out.println("Tempo é: " + horas + " horas e " + minutos+" minutos");
+
+	}
+}
