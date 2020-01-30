b82a728125b2bb976b3c1e7e1df8c8c487d0a352
Treinar <Treinar@MICRO131>
2020-01-29 22:26:40 -0200

    add atividade

---
+++ luciano/atividade/Atividade/src/atividade.java
@@ -0,0 +1,15 @@
+public class atividade {
+
+	public static void main(String[] args) {
+
+		int a = 888;
+		int b = 60;
+		int hora = a / b;
+		int minuto = a % b;
+
+		System.out.println(" a hora é  " + hora + ":" + minuto);
+	}
+}
+// o % é para descobrir o mod (modulo)
+
+
