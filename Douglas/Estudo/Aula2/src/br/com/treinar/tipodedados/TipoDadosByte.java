7bc1561b1bab9633492b5b65c630c75cd1322237
Treinar <Treinar@MICRO>
2020-01-29 22:45:08 -0200

    Novo

---
+++ Douglas/Estudo/Aula2/src/br/com/treinar/tipodedados/TipoDadosByte.java
@@ -0,0 +1,12 @@
+package br.com.treinar.tipodedados;
+/**
+ * 
+ * @author Treinar
+ *
+ */
+public class TipoDadosByte {
+	public static void main(String[] args) {
+		short s = 218;
+		System.out.println("s = " + s);
+	}
+}
