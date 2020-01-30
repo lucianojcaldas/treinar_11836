a9c7d52502b990830c8d66a23c6f960b1799b19c
Treinar <Treinar@MICRO>
2020-01-29 22:44:11 -0200

    Novo

---
+++ Douglas/Estudo/aula3/src/br/com/treinar/repeticao/RepeticaoWhile.java
@@ -0,0 +1,11 @@
+package br.com.treinar.repeticao;
+
+public class RepeticaoWhile {
+	public static void main(String[] args) {
+		int contador = 0;
+		while (contador < 10) {
+			System.out.println("contador vale: " + contador);
+			contador++;
+		}
+	}
+}
