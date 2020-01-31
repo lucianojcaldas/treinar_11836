ecb9b0d0dd348bcb28663133bfde4d920cb6f5cc
Treinar <Treinar@10.2.0.151>
2020-01-31 20:46:33 -0200

    Implementando dois métodos

--- professor/base/banco-inter/src/br/com/bancointer/model/core/Conta.java
+++ professor/base/banco-inter/src/br/com/bancointer/model/core/Conta.java
@@ -5,8 +5,18 @@ import br.com.bancointer.model.Cliente;
 public class Conta {
 
 	public Double saldo;
+	public Double limiteCredito;
 	public Integer numero;
 	public Integer agencia;
 	public Cliente cliente;
+
+	public void depositar(Double valorDeposito) {
+		saldo += valorDeposito;
+
+	}
+
+	public double recuperarSaldo() {
+		return saldo + limiteCredito;
+	}
 
 }
--- professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
+++ professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
@@ -12,7 +12,7 @@ public class TelaBancoInter {
 		Integer opcao;
 		Conta c = null;
 		do {
-			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n=> ");
+			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n\t3 - Depositar\n=> ");
 			opcao = teclado.nextInt();
 			switch (opcao) {
 			case 0:
@@ -24,8 +24,10 @@ public class TelaBancoInter {
 				c.numero = teclado.nextInt();
 				System.out.print("Informe o numero da agencia: ");
 				c.agencia = teclado.nextInt();
-				System.out.print("Informe o saldo da conta: ");
+				System.out.print("Informe o Depósito inicial da conta: ");
 				c.saldo = teclado.nextDouble();
+				System.out.println("Informe o limite de Crédito Inicial: ");
+				c.limiteCredito = teclado.nextDouble();
 				System.out.println("Conta criada com sucesso.");
 				System.out.println("\n<----------------->\n");
 				break;
@@ -33,9 +35,21 @@ public class TelaBancoInter {
 				if (c == null) {
 					System.out.println("Por favor crie a conta antes.");
 				} else {
-					System.out.println("Saldo atual: " + c.saldo);
+					System.out.println("Saldo atual: " + c.recuperarSaldo());
 				}
 				System.out.println("\n<----------------->\n");
+				break;
+			case 3:
+				if (c == null) {
+					System.out.println("Por favor crie a conta antes.");
+				} else {
+
+					System.out.println("Informe o valor do depósito a ser feito: ");
+					c.depositar(teclado.nextDouble());
+					System.out.println("Depósito feito com sucesso!");
+				}
+				System.out.println("\n<----------------->\n");
+
 				break;
 			default:
 				System.out.println("Código inválido.");
