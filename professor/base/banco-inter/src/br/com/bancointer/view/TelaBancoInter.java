390b6c55ec756610a0a618fc299015bba8d83b54
ADM <ADM@10.2.0.100>
2020-01-30 22:44:05 -0200

    Menu criar conta

--- professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
+++ professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
@@ -2,15 +2,33 @@ package br.com.bancointer.view;
 
 import java.util.Scanner;
 
+import br.com.bancointer.model.core.Conta;
+
 public class TelaBancoInter {
 
 	Scanner teclado = new Scanner(System.in);
-	
+
 	public void iniciar() {
 		Integer opcao;
+		Conta c = null;
 		do {
-			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n=> ");
+			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n=> ");
 			opcao = teclado.nextInt();
+			switch (opcao) {
+			case 1:
+				c = new Conta();
+				System.out.print("Informe o numero da conta: ");
+				c.numero = teclado.nextInt();
+				System.out.print("Informe o numero da agencia: ");
+				c.agencia = teclado.nextInt();
+				System.out.print("Informe o saldo da conta: ");
+				c.saldo = teclado.nextDouble();
+				break;
+			case 2:
+				System.out.println("Saldo atual: " + c.saldo);
+			default:
+				break;
+			}
 		} while (!opcao.equals(0));
 	}
 
