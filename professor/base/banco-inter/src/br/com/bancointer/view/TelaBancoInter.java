68538930afd940dc0eebf2dd4ea0f1e7a3fa93c1
Treinar <Treinar@10.2.0.151>
2020-01-31 21:40:30 -0200

    Pausa cafe

--- professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
+++ professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
@@ -12,43 +12,20 @@ public class TelaBancoInter {
 		Integer opcao;
 		Conta c = null;
 		do {
-			System.out.print("Informe:\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n\t3 - Depositar\n=> ");
+			System.out.println(recuperarMenu());
 			opcao = teclado.nextInt();
 			switch (opcao) {
 			case 0:
 				System.out.println("\n<----------------->\n");
 				break;
 			case 1:
-				c = new Conta();
-				System.out.print("Informe o número da conta: ");
-				c.numero = teclado.nextInt();
-				System.out.print("Informe o número da agencia: ");
-				c.agencia = teclado.nextInt();
-				System.out.print("Informe o Depósito inicial da conta: ");
-				c.saldo = teclado.nextDouble();
-				System.out.println("Informe o limite de Crédito Inicial: ");
-				c.limiteCredito = teclado.nextDouble();
-				System.out.println("Conta criada com sucesso.");
-				System.out.println("\n<----------------->\n");
+				c = criarConta();
 				break;
 			case 2:
-				if (c == null) {
-					System.out.println("Por favor crie a conta antes.");
-				} else {
-					System.out.println("Saldo atual: " + c.recuperarSaldo());
-				}
-				System.out.println("\n<----------------->\n");
+				exibirSaldo(c);
 				break;
 			case 3:
-				if (c == null) {
-					System.out.println("Por favor crie a conta antes.");
-				} else {
-
-					System.out.println("Informe o valor do depósito a ser feito: ");
-					c.depositar(teclado.nextDouble());
-					System.out.println("Depósito feito com sucesso!");
-				}
-				System.out.println("\n<----------------->\n");
+				fazerDeposito(c);
 
 				break;
 			default:
@@ -57,6 +34,47 @@ public class TelaBancoInter {
 				break;
 			}
 		} while (!opcao.equals(0));
+	}
+
+	public String recuperarMenu() {
+		return "Informe:\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n\t3 - Depositar\n=> ";
+	}
+
+	private Conta criarConta() {
+
+		Conta c = new Conta();
+		System.out.print("Informe o número da conta: ");
+		c.numero = teclado.nextInt();
+		System.out.print("Informe o número da agencia: ");
+		c.agencia = teclado.nextInt();
+		System.out.print("Informe o Depósito inicial da conta: ");
+		c.saldo = teclado.nextDouble();
+		System.out.println("Informe o limite de Crédito Inicial: ");
+		c.limiteCredito = teclado.nextDouble();
+		System.out.println("Conta criada com sucesso.");
+		System.out.println("\n<----------------->\n");
+		return c;
+	}
+
+	private void exibirSaldo(Conta conta) {
+		if (conta == null) {
+			System.out.println("Por favor crie a conta antes.");
+		} else {
+			System.out.println("Saldo atual: " + conta.recuperarSaldo());
+		}
+		System.out.println("\n<----------------->\n");
+	}
+
+	private void fazerDeposito(Conta conta) {
+		if (conta == null) {
+			System.out.println("Por favor crie a conta antes.");
+		} else {
+
+			System.out.println("Informe o valor do depósito a ser feito: ");
+			conta.depositar(teclado.nextDouble());
+			System.out.println("Depósito feito com sucesso!");
+		}
+		System.out.println("\n<----------------->\n");
 	}
 
 }
