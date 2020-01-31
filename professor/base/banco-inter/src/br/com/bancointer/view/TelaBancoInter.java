c6e7e3d7de947c16cf42979d045c14dda12508cb
Treinar <Treinar@MICRO60>
2020-01-30 22:55:14 -0200

    meu prj

--- professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
+++ professor/base/banco-inter/src/br/com/bancointer/view/TelaBancoInter.java
@@ -2,15 +2,53 @@ package br.com.bancointer.view;
 
 import java.util.Scanner;
 
+import br.com.bancointer.model.Cliente;
+import br.com.bancointer.model.core.Conta;
+
 public class TelaBancoInter {
 
 	Scanner teclado = new Scanner(System.in);
-	
+
 	public void iniciar() {
 		Integer opcao;
+		Conta conta;
+		conta = null;
 		do {
-			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n=> ");
+			System.out.print("Informe\n\t0 - Sair\n\t1 - Criar Conta\n\t2 - Exibir Saldo\n=> ");
 			opcao = teclado.nextInt();
+
+			switch (opcao) {
+			case 1:
+
+				// Criando o Cliente
+				Cliente cliente = new Cliente();
+				System.out.print("Digite o nome do Cliente: ");
+				String nomeCliente = teclado.next();
+				cliente.nome = nomeCliente;
+				System.out.print("Digite a idade: ");
+				int idadeCliente = teclado.nextInt();
+				cliente.idade = idadeCliente;
+
+				// Criando a Conta
+				conta = new Conta();
+				System.out.print("Informe o numero da conta: ");
+				conta.numero = teclado.nextInt();
+				conta.cliente = cliente;
+				System.out.print("Informe o numero da agencia: ");
+				conta.agencia = teclado.nextInt();
+				conta.saldo = 0d;
+				String nome = conta.cliente.nome;
+				int idade = conta.cliente.idade;
+
+				System.out.println("Cliente: " + nome + "\n Idade: " + idade + "\n CC: " + conta.numero + "\n AG: "
+						+ conta.agencia + "\n Saldo: " + conta.saldo + "\n------------------------\n");
+				break;
+			case 2:
+				System.out.println("O Saldo da conta é: " + conta.saldo + "\n------------------------\n");
+				break;
+			default:
+				break;
+			}
 		} while (!opcao.equals(0));
 	}
 
