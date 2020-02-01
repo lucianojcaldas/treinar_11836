0d01f8aa23f2c6d1b907b2261f9e1ffc509c76b0
Treinar <Treinar@MICRO35>
2020-01-31 23:09:14 -0200

    alteracao

--- ricardo/projeto3/tipos de operadores/src/br/com/treinar/classe/AnaliseDeCredito.java
+++ ricardo/projeto3/tipos de operadores/src/br/com/treinar/classe/AnaliseDeCredito.java
@@ -2,4 +2,10 @@ package br.com.treinar.classe;
 
 public class AnaliseDeCredito {
 
+	public String Analise(){
+
+		System.out.println(" credito autorizado");
+		System.out.println(" credito nao autorizado");
+		 
+	}
 }
